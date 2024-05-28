9ece5bea1d24d5578071ea86149b7334065b48bd
Nazia <nazia.jafrin1@gmail.com>
2024-05-28 13:14:14 +0600

    3rd commit

---
+++ All of JS/variables.js
@@ -0,0 +1,16 @@
+var name = 'Hasibul Hasan'
+var id = 192041
+
+
+// better naming 
+var stuName = 'Nazia Jafrin'
+var stuId = 192042
+
+console.log('student name is ' + name)
+console.log('id of the student is ' + id)
+console.log(name + " and " + id) 
+
+
+console.log('student name is ' + stuName)
+console.log('id of the student is ' + stuId)
+console.log(stuName + " and " + stuId)
