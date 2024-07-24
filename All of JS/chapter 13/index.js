4c4a8cb42213e888ae0d00b1116ed67e52e1c333
Nazia <nazia.jafrin1@gmail.com>
2024-07-24 21:58:34 +0600

    constructor prototype

---
+++ All of JS/chapter 13/index.js
@@ -0,0 +1,31 @@
+//.....................................
+// Inheritance
+// ...... ...... ....................
+
+
+function Person(name){ // constructor
+  this.name = name
+}
+var p1 = new Person('Nazia Jafrin')
+console.log(p1)
+
+function Students (roll){
+  // return studentRoll(roll){
+  //   this.roll = roll
+  // }
+}
+
+var student = new Students(23)
+
+// var Person = {
+//   name : ' Nazia Jafrin'
+// }
+// console.log(Person)
+
+// for(var i in Person){
+//   console.log(i)
+// }
+// console.log(Object.keys(Person))
+
+// var descriptor = Object.getOwnPropertyDescriptor(Person,'name')
+// console.log(descriptor)
